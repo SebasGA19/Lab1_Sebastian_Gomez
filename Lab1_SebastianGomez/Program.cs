191688785eca1f19980bd46fc4449c723ca079a6
SebasGA19 <62715839+SebasGA19@users.noreply.github.com>
2021-01-19 17:40:47 -0500

    Primer commit

---
+++ Lab1_SebastianGomez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab1_SebastianGomez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
